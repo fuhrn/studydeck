c6b5401f07695e16ab14ef2509d482572e8a014f
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-05 18:41:46 -0300

    Apply Styles to a UI in JSX

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from "react";
+import "./normalize.css";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <header>
-        <h1>StudyDeck</h1>
+        <h1>
+          Study <span className="titleHighlight">Deck</span>
+        </h1>
         <h2>Retention through repetition</h2>
       </header>
       <main>
         <h3>Your Cards</h3>
-        <div>
-          <div>
-            <h4>Term goes here</h4>
-            <div>
-              <button type="button">Show back</button>
-              <button type="button">Edit</button>
-              <button type="button">delete</button>
+        <div className="gridContainer">
+          <div className="tile">
+            <h4 className="cardTerm">Term goes here</h4>
+            <div className="cardButtons">
+              <button type="button" className="tertiary">
+                Show back
+              </button>
+              <div>
+                <button type="button" className="secondary">
+                  Edit
+                </button>
+                <button type="button" className="secondary danger">
+                  delete
+                </button>
+              </div>
             </div>
           </div>
         </div>
