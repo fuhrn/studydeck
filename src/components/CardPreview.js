448795d4470f3d8e119068d3caef911106f07532
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-05 19:14:04 -0300

    Maintain State in a React Component with the useState Hook

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
-export function CardPreview(props) {
+export function CardPreview({ term, definition }) {
+  const [isFront, setIsFront] = useState(true)
+  function handleCardFlip() {
+    setIsFront(current => !current)
+  }
   return (
-    <div className="tile">
-      <h4 className="cardTerm">{props.term}</h4>
+    <div className={`tile ${isFront ? '' : 'back'}`} >
+      <h4 className="cardTerm">{isFront ? term : definition}</h4>
       <div className="cardButtons">
-        <button type="button" className="tertiary">
-          Show back
+        <button type="button" className="tertiary" onClick={handleCardFlip}>
+          {isFront ? "Show back" : "Show Front"}
         </button>
         <div>
           <button type="button" className="secondary">
