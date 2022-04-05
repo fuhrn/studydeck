12b9032d9b2667a83b06e4e788e92ab445130d47
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-05 20:18:53 -0300

    Test Component Interaction with Jest and Testing Library

--- src/components/CardPreview.spec.js
+++ src/components/CardPreview.spec.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { render } from '@testing-library/react'
+import { render , fireEvent, queryByText} from '@testing-library/react'
 import { CardPreview } from "./CardPreview";
 
 it('Renders the expected term', () => {
   const expectedTerm = 'this is a test'
-  const { getByText } = render(<CardPreview term={expectedTerm} />)
-  expect(getByText(expectedTerm)).toBeInTheDocument
+  const { getByText, queryByText } = render(<CardPreview term={expectedTerm} />)
+  expect(getByText(expectedTerm)).toBeInTheDocument()
 }) 
+
+it("Flips the card to reveal the definition", () => {
+  const expectedTerm = "this is the term";
+  const expectedDef = "this is the definition";
+  const { getByText } = render(
+    <CardPreview term={expectedTerm} definition={expectedDef} />
+  );
+
+  expect(getByText(expectedTerm)).toBeInTheDocument;
+
+  const flipButton = getByText(/show/i);
+  fireEvent.click(flipButton);
+
+  expect(getByText(expectedDef)).toBeInTheDocument();
+  //expect(queryByText(expectedTerm)).not.toBeInTheDocument(); //TypeError: container.querySelectorAll is not a function
+}); 
